e5b83a1093cb7f8da2fb705bb33de6e751894e22
Adam Tapper <adam@tapper.one>
2021-02-23 21:27:16 +0100

    adds In-Game menu scene

--- Assets/Scripts/HUD/InGameMenu.cs
+++ Assets/Scripts/HUD/InGameMenu.cs
@@ -1,14 +1,28 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class InGameMenu : MonoBehaviour {
-    public Button ContinueButton;
-    public Button RestartButton;
-    public Button MainMenuButton;
-    public Button CancelButton;
+namespace HUD {
+    public class InGameMenu : MonoBehaviour {
+        public Button ContinueButton;
+        public Button RestartButton;
+        public Button MainMenuButton;
+        public Button CancelButton;
 
+        public void Toggle(GameObject gameObject) {
+            gameObject.SetActive(!gameObject.activeSelf);
+        }
     
+    
+        public void RestartMenu() {
+        
+            if (RestartButton) {
+                SceneManager.LoadScene("Scenes/SampleScene");
+            }
+        }
 
+        public void OnClick() {
+        
+        }
+    }
 }
