cda8660d366cbd93fc35c22f5cedce3c497613d1
Adam Tapper <adam@tapper.one>
2021-02-24 15:15:03 +0100

    adds script for DeathMenu and small changes to InGameMenu

---
+++ Assets/Scripts/HUD/DeathMenu.cs
@@ -0,0 +1,5 @@
+namespace HUD {
+    public class DeathMenu {
+        
+    }
+}
--- Assets/Scripts/HUD/InGameMenu.cs
+++ Assets/Scripts/HUD/InGameMenu.cs
@@ -1,14 +1,22 @@
+using Player.Energy;
 using UnityEngine;
 
 namespace HUD {
     public class InGameMenu : MonoBehaviour {
+
+        public int energy;
         
         public void Toggle(GameObject gameObject) {
             gameObject.SetActive(!gameObject.activeSelf);
         }
 
-        public void ContinueButton() {
+        private void ContinueButton() {
+            //return player to full health
+            //continue only if energy is not 0
             
+            if (energy > 1) {
+                
+            }
         }
 
         public void RestartButton() {
