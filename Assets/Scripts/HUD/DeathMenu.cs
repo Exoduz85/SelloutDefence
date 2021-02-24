02964fe1dda097447cccadf5d8d0facaa8acd18b
Adam Tapper <adam@tapper.one>
2021-02-24 16:42:21 +0100

    DeathMenu became DefeatMenu

--- Assets/Scripts/HUD/DeathMenu.cs
+++
@@ -1,5 +0,0 @@
-namespace HUD {
-    public class DeathMenu {
-        
-    }
-}
---
+++ Assets/Scripts/HUD/DefeatMenu.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace HUD {
+    public class DefeatMenu {
+        //when a player dies
+        //this menu shows up
+        //Continue / Restart / MainMenu
+
+        public int playerHealth;
+
+        public void OnDefeat() {
+            
+        }
+
+    }
+}
