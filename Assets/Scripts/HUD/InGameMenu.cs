acc8b3f5f618174837c729acac3184def9fcfba9
Adam Tapper <adam@tapper.one>
2021-02-24 14:15:03 +0100

    no message

--- Assets/Scripts/HUD/InGameMenu.cs
+++ Assets/Scripts/HUD/InGameMenu.cs
@@ -1,28 +1,27 @@
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
 namespace HUD {
     public class InGameMenu : MonoBehaviour {
-        public Button ContinueButton;
-        public Button RestartButton;
-        public Button MainMenuButton;
-        public Button CancelButton;
-
+        
         public void Toggle(GameObject gameObject) {
             gameObject.SetActive(!gameObject.activeSelf);
         }
-    
-    
-        public void RestartMenu() {
-        
-            if (RestartButton) {
-                SceneManager.LoadScene("Scenes/SampleScene");
-            }
+
+        public void ContinueButton() {
+            
         }
 
-        public void OnClick() {
-        
+        public void RestartButton() {
+            
         }
+
+        public void MainMenuButton() {
+            
+        }
+
+        public void CancelButton() {
+            
+        }
+        
     }
 }
