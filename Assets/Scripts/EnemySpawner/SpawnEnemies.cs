1c06ef1edf03a1b5fea9325081c4cee906c7e42d
Joel Ring <joel.ring@gmail.com>
2021-02-25 11:33:48 +0100

    adds enemyWave scriptable objects. tidy up folder structure

---
+++ Assets/Scripts/EnemySpawner/SpawnEnemies.cs
@@ -0,0 +1,38 @@
+using EnemySpawner;
+using UnityEngine;
+
+public class SpawnEnemies : MonoBehaviour{
+    [SerializeField] private EnemyWave _enemyWaves;
+
+    private float _spawnInterval;
+    private int _numberOfEnemies;
+    private float _enemySpeed;
+    private GameObject _enemyPrefab;
+
+    private float spawnTimer;
+    private int enemyNumber;
+
+    void Start(){
+        _spawnInterval = _enemyWaves.spawnInterval;
+        _numberOfEnemies = _enemyWaves.numberOfEnemies;
+        _enemySpeed = _enemyWaves.enemySpeed;
+        _enemyPrefab = _enemyWaves.enemyPrefab;
+    }
+    
+    void Update(){
+        spawnTimer += Time.deltaTime;
+
+        if (spawnTimer >= _spawnInterval){
+            spawnTimer = 0;
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy(){
+        if (enemyNumber > _numberOfEnemies)
+            return;
+        Debug.Log($"spawning new enemy: {enemyNumber}");
+        enemyNumber++;
+        
+    }
+}
