0abd63242838ef30bfd2092743a4cf502fda4dbf
Adam Tapper <adam@tapper.one>
2021-02-25 13:42:27 +0100

    adds prefab for MenuButtonHolder

--- Assets/Scripts/HUD/DefeatMenu.cs
+++ Assets/Scripts/HUD/DefeatMenu.cs
@@ -1,15 +1,20 @@
 using System;
+using UnityEngine;
 
 namespace HUD {
     public class DefeatMenu {
         //when a player dies
         //this menu shows up
-        //Continue / Restart / MainMenu
+        //Continue(costs 1 coin or smth) / Restart / MainMenu
 
-        public int playerHealth;
+        public int towerHealth;
 
         public void OnDefeat() {
-            
+            ShowMenu(gameObject);
+        }
+
+        public void ShowMenu(GameObject gameObject) {
+            gameObject.SetActive(!gameObject.activeSelf);
         }
 
     }
--- Assets/Scripts/HUD/InGameMenu.cs
+++ Assets/Scripts/HUD/InGameMenu.cs
@@ -3,20 +3,9 @@ using UnityEngine;
 
 namespace HUD {
     public class InGameMenu : MonoBehaviour {
-
-        public int energy;
         
         public void Toggle(GameObject gameObject) {
             gameObject.SetActive(!gameObject.activeSelf);
-        }
-
-        private void ContinueButton() {
-            //return player to full health
-            //continue only if energy is not 0
-            
-            if (energy > 1) {
-                
-            }
         }
 
         public void RestartButton() {
