f4fad0606d45de2387fc664d3fd71d5d4642ab9b
Alex Rynkowski <alex.rynkowski90@gmail.com>
2021-03-01 14:31:32 +0100

    uncomments Broker.Instanace() in PlayGame.cs

--- Assets/Scripts/HUD/PlayGame.cs
+++ Assets/Scripts/HUD/PlayGame.cs
@@ -1,10 +1,12 @@
-﻿using Player.Energy;
+﻿using Core;
+using EventBrokerFolder;
+using Player.Energy;
 using UnityEngine;
 
 namespace HUD {
     public class PlayGame : MonoBehaviour {
         public void Play(string sceneToLoad) {
-            //EventBroker.Instance().SendMessage(new EventLoadScene(sceneToLoad));
+            EventBroker.Instance().SendMessage(new EventLoadScene(sceneToLoad));
         }
 
         public void ConsumeEnergy(Energy energy) {
