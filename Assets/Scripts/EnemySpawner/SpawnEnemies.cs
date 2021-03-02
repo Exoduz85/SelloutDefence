3b5270d98cd0fc6c7f3c2377c4bd996fad62beab
Alex Rynkowski <alex.rynkowski90@gmail.com>
2021-03-02 14:41:06 +0100

    update for the enemyspawner (option to make the wave infinite)

--- Assets/Scripts/EnemySpawner/SpawnEnemies.cs
+++ Assets/Scripts/EnemySpawner/SpawnEnemies.cs
@@ -2,17 +2,20 @@ using System.Collections.Generic;
 using Enemy;
 using UnityEngine;
 
-namespace EnemySpawner{
-    public class SpawnEnemies : MonoBehaviour{
-        [SerializeField] private EnemyWave[] _enemyWaves;
+namespace EnemySpawner {
+    public class SpawnEnemies : MonoBehaviour {
+        [SerializeField]
+        private EnemyWave[] _enemyWaves;
+        [SerializeField]
+        private bool shouldRepeat;
 
-        private class WaveData{
+        private class WaveData {
             public readonly float SpawnInterval;
             public readonly int NumberOfEnemies;
             public readonly float EnemySpeed;
             public readonly GameObject EnemyPrefab;
 
-            public WaveData(float spawnInterval, int noEnemies, float speed, GameObject prefab){
+            public WaveData(float spawnInterval, int noEnemies, float speed, GameObject prefab) {
                 SpawnInterval = spawnInterval;
                 NumberOfEnemies = noEnemies;
                 EnemySpeed = speed;
@@ -22,43 +25,51 @@ namespace EnemySpawner{
 
         private readonly List<WaveData> _wavesList = new List<WaveData>();
 
-        private float spawnTimer; 
+        private float spawnTimer;
         private int waveNumber;
         private int enemyInWaveNumber;
 
-        void Awake(){
-            foreach (var wave in _enemyWaves){
-                _wavesList.Add(new WaveData(wave.spawnInterval,wave.numberOfEnemies,wave.enemySpeed, wave.enemyPrefab ));
+        void Awake() {
+            foreach (var wave in _enemyWaves) {
+                _wavesList.Add(new WaveData(wave.spawnInterval, wave.numberOfEnemies, wave.enemySpeed, wave.enemyPrefab));
             }
         }
-    
-        void Update(){
+
+        void Update() {
             //we have traversed all waves
-            if (waveNumber >= _wavesList.Count)
-                return;
-            
-            if (isReadyToSpawn()){
+            if (waveNumber >= _wavesList.Count) {
+                if (this.shouldRepeat) {
+                    this.waveNumber = 0;
+                    this.enemyInWaveNumber = 0;
+                }
+                else {
+                    return;
+                }
+            }
+
+            if (isReadyToSpawn()) {
                 SpawnEnemy();
                 enemyInWaveNumber++;
             }
-            
+
             //we have traversed all enemies in the current wave
-            if (enemyInWaveNumber >= _wavesList[waveNumber].NumberOfEnemies){
+            if (enemyInWaveNumber >= _wavesList[waveNumber].NumberOfEnemies) {
                 waveNumber++;
                 enemyInWaveNumber = 0;
             }
         }
 
-        private bool isReadyToSpawn(){
+        private bool isReadyToSpawn() {
             spawnTimer += Time.deltaTime;
-            if (spawnTimer >= _wavesList[waveNumber].SpawnInterval){
+            if (spawnTimer >= _wavesList[waveNumber].SpawnInterval) {
                 spawnTimer = 0;
                 return true;
             }
+
             return false;
         }
 
-        private void SpawnEnemy(){
+        private void SpawnEnemy() {
             var newEnemy = Instantiate(_wavesList[waveNumber].EnemyPrefab, transform.position, Quaternion.identity, transform);
             newEnemy.GetComponent<MoveEnemy>().speed = _wavesList[waveNumber].EnemySpeed;
             newEnemy.name += enemyInWaveNumber;
