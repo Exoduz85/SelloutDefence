14a5276ccdc7c42b2ce1a4a687d6f44835f99cd9
Michael Van Der Poll <michaelvdp@gmail.com>
2021-03-02 09:51:07 +0100

    Ads monetization things

--- Assets/Scripts/InAppPurchases/IStore.cs
+++
@@ -1,9 +0,0 @@
-public interface IStore {
-    
-    //IAP requires products of 2 variants
-    //Consumable(limited amount of purchases, eg. Disable Ads purchase should only be able to happen once)
-    //Non-Consumable - such as boosters
-    
-    public bool purchase();
-    
-}
