4646b5c596913a10912c64f2b539ef260ba506d8
Adam Tapper <adam@tapper.one>
2021-03-03 17:06:00 +0100

    finalize the PlayWave button

--- Assets/Scripts/HUD/PlayWave.cs
+++ Assets/Scripts/HUD/PlayWave.cs
@@ -9,22 +9,28 @@ using UnityEngine.UI;
 namespace HUD {
     public class PlayWave : MonoBehaviour {
         public Text timerCountDownText;
-        public bool active;
+        public float countDownStart = 10f;
+        string buttonStartText;
+
+        public void Start() {
+            buttonStartText = timerCountDownText.text;
+        }
 
         IEnumerator countDown(float countDownStartTime) {
             var spaceTMP = countDownStartTime;
             while (spaceTMP > 0) {
                 spaceTMP -= Time.deltaTime;
                 timerCountDownText.text = spaceTMP.ToString("0");
+                yield return null;
             }
             GetComponent<Button>().interactable = true;
-            yield return null;
+            timerCountDownText.text = buttonStartText;
         }
 
         public void playWaveButton() {
             GetComponent<Button>().interactable = false;
             EventBroker.Instance().SendMessage(new EventStartWave(true));
-            StartCoroutine(countDown(10));
+            StartCoroutine(countDown(countDownStart));
         }
     }
 }
