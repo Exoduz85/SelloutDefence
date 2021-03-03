1fe84eec31b683d4abd4b67a388396e06295e6b4
Fredrik Wallander <f.m.wallander@gmail.com>
2021-03-03 15:21:29 +0100

    Adds corrected enemies
    wrongly used the old prefab to make new enemies.

--- Assets/Scripts/Enemy/ScaleEnemy.cs
+++ Assets/Scripts/Enemy/ScaleEnemy.cs
@@ -1,22 +1,23 @@
+using System;
 using UnityEngine;
 
 public class ScaleEnemy : MonoBehaviour{
     private float scalingSpeed;
-    private float scale;
+    private float scale = 2f;
     private bool scaleDown;
     void Update(){
         scale += scalingSpeed;
         this.transform.localScale = new Vector2(scale, scale);
-        if (this.transform.localScale.x > 2.5f){
+        if (this.transform.localScale.x > 3f){
             scaleDown = true;
         }
-        else if (this.transform.localScale.x < 1.5f){
+        else if (this.transform.localScale.x < 2f){
             scaleDown = false;
         }
         if (scaleDown){
-            scalingSpeed = -0.02f;
+            scalingSpeed = -0.03f;
         }else if (!scaleDown){
-            scalingSpeed = 0.02f;
+            scalingSpeed = 0.03f;
         }
     }
 }
