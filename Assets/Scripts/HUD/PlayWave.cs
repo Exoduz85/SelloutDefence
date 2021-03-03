80b80cf3896cdd11b4b0fe8d5e8415c90e5ad0b6
Adam Tapper <adam@tapper.one>
2021-03-03 16:44:19 +0100

    changes to PlayWave script

--- Assets/Scripts/HUD/PlayWave.cs
+++ Assets/Scripts/HUD/PlayWave.cs
@@ -1,14 +1,30 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Enemy;
 using EventBrokerFolder;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace HUD {
-    public class PlayWave : MonoBehaviour
-    {
+    public class PlayWave : MonoBehaviour {
+        public Text timerCountDownText;
+        public bool active;
+
+        IEnumerator countDown(float countDownStartTime) {
+            var spaceTMP = countDownStartTime;
+            while (spaceTMP > 0) {
+                spaceTMP -= Time.deltaTime;
+                timerCountDownText.text = spaceTMP.ToString("0");
+            }
+            GetComponent<Button>().interactable = true;
+            yield return null;
+        }
+
         public void playWaveButton() {
+            GetComponent<Button>().interactable = false;
             EventBroker.Instance().SendMessage(new EventStartWave(true));
+            StartCoroutine(countDown(10));
         }
     }
 }
