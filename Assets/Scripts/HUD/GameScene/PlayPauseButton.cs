adec976e0f0562b59a806ebaa6b261fab56cebdf
Adam Tapper <adam@tapper.one>
2021-03-04 23:19:11 +0100

    adds PlayPauseButton

--- Assets/Scripts/HUD/GameScene/PlayPauseButton.cs
+++ Assets/Scripts/HUD/GameScene/PlayPauseButton.cs
@@ -1,21 +1,38 @@
-using System.Collections;
-using System.Collections.Generic;
+
+
 using UnityEngine;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
 
-public class PlayPauseButton : MonoBehaviour
-{
-    public class FastForward : MonoBehaviour {
+public class PlayPauseButton : MonoBehaviour {
         public Button playPauseButton;
-        public Image play;
-        public Image pause;
+        public GameObject play;
+        public GameObject pause;
+
+        public void Start() {
+            play = GameObject.FindGameObjectWithTag("play");
+            pause = GameObject.FindGameObjectWithTag("pause");
+        }
+
+        public void TogglePlayPauseImage() {
+            if (Time.timeScale != 0) {
+                play.SetActive(true);
+            }
+            else if (Time.timeScale >= 1) {
+                play.SetActive(false);
+                pause.SetActive(true);
+            }
+}
 
         public void TogglePlayPause() {
             Time.timeScale = Time.timeScale == 1f ? 0f : 1f;
-            if (Time.timeScale >= 0) {
-                play.enabled = pause.enabled != true;
+            if (Time.timeScale == 0) {
+                play.SetActive(false);
+                pause.SetActive(true);
+            }
+            else if (Time.timeScale >= 1) {
+                pause.SetActive(false);
+                play.SetActive(true);
             }
         }
-    }
+    
 }
