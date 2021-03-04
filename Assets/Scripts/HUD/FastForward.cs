ba13f91d80d6830847381affc1dd6744d065d0f2
Adam Tapper <adam@tapper.one>
2021-03-04 10:46:49 +0100

    changes to iternary in FastForward script

--- Assets/Scripts/HUD/FastForward.cs
+++ Assets/Scripts/HUD/FastForward.cs
@@ -1,19 +1,13 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class FastForward : MonoBehaviour {
-    public Button fastForwardButton;
-    public Text fastForwardText;
+namespace HUD {
+    public class FastForward : MonoBehaviour {
+        public Button fastForwardButton;
+        public Text fastForwardText;
 
-    public void Toggle() {
-        if (Time.timeScale == 1f) {
-            Time.timeScale = 2f;
-            fastForwardText.text = Time.timeScale.ToString(Time.timeScale + "x");
-        } else {
-            Time.timeScale = 1f;
+        public void Toggle() {
+            Time.timeScale = Time.timeScale == 1f ? 2f : 1f;
             fastForwardText.text = Time.timeScale.ToString(Time.timeScale + "x");
         }
     }
