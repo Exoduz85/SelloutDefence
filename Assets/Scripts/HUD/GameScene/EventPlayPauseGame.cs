a244cd9fde08e190f5d4d979d921d59878f23e12
Adam Tapper <adam@tapper.one>
2021-03-05 12:27:17 +0100

    adds EventPlayPauseGame

---
+++ Assets/Scripts/HUD/GameScene/EventPlayPauseGame.cs
@@ -0,0 +1,9 @@
+namespace HUD.GameScene {
+    public class EventPlayPauseGame {
+        public bool isGamePaused;
+
+        public EventPlayPauseGame(bool isGamePaused) {
+            this.isGamePaused = isGamePaused;
+        }
+    }
+}
